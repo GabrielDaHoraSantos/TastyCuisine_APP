65fd01b50547cc6066ed9432c50b328c2c5bfc07
GabrielDaHoraSantos <dahorasanyosgabriel@gmail.com>
2026-03-12 16:30:20 -0300

    Primeiro 0.1

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,7 @@
 import * as Device from 'expo-device';
 import { Platform, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import 'react-native-gesture-handler';
 
 import { AnimatedIcon } from '@/components/animated-icon';
 import { HintRow } from '@/components/hint-row';
