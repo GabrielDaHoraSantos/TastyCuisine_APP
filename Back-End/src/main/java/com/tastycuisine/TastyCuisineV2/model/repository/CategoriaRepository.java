a6021e3f70b784f151fc28ee099cfe341afe16f5
KAIO-BASTOS-ALVES <rm95196@estudante.fieb.edu.br>
2026-06-25 09:08:26 -0300

    Back-End

--- Back-End/src/main/java/com/tastycuisine/TastyCuisineV2/model/repository/CategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tastycuisine.TastyCuisineV2.model.repository;
-
-import com.tastycuisine.TastyCuisineV2.model.entity.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-}
