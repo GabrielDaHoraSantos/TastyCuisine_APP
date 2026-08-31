2c7bd2a52b8e6c8d9096ff65a5eabcf78d59dbd2
Kaio-Bastos-Alves <rm95196@estudante.fieb.edu.br>
2026-08-31 17:35:45 -0300

    alterei o subapase pra funfar

--- app/(services)/subapase.js
+++ app/(services)/subapase.js
@@ -3,14 +3,14 @@ import * as FileSystem from 'expo-file-system';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
-import { AppState } from 'react-native';
+import { AppState, Platform } from 'react-native';
 
 const supabaseUrl = 'https://xphzvvxmwecnsampbnmx.supabase.co';
 const supabaseAnonKey = 'sb_publishable_xznx0wX72X85jhndiuGsZg_ZVovmcwI';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
-    storage: AsyncStorage,
+    storage: Platform.OS === 'web' && typeof window === 'undefined' ? undefined : AsyncStorage,
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: false,
